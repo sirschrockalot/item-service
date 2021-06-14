9eaf5172dd0e778d556c725e63d0541d699ee32e
Joel Schrock <joel.schrock@gmail.com>
2021-06-13 20:52:05 -0500

    added back CORS

--- src/main/java/com/items/itemservice/ItemServiceApplication.java
+++ src/main/java/com/items/itemservice/ItemServiceApplication.java
@@ -15,6 +15,7 @@ import com.google.gson.GsonBuilder;
 @SpringBootApplication
 @RestController
 //@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RequestMapping(value = "/items", produces = { "application/json" })
 public class ItemServiceApplication {
 	@Autowired
